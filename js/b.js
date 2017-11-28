bf5118bfaa415deaf8d3e5b6f0e2a9df9df32c29
Code <soaresss@appstate.edu>
2017-11-28 17:35:01 -0500

    Finished optimization

--- js/b.js
+++ js/b.js
@@ -1,23 +1,32 @@
 $(document).ready(function(){
-	getStorage();
 	initializeFields();
+	initializeTotal();
 	$("#tabb4 button").on('click',function(event){
 		addNewRowGroup4('#tabb4');
+		setStorage('tabb4');
 	});
 	$("button").not('#tabb4').on('click', function(event) {
 		console.log("adding a new row --" + " user pressed button id " + event.target.id);
 		addNewRow(event);		/* Act on the event */
-		setStorage();
+		setStorage(event.target.id);
 	});
 	$("#tabb4").on('keypress keyup blur change load','input',function(event){
 		$("#tabb4"+ " span").text('$ ' + getCost()); //Set total to tabletotal span tag
 	});
 	$("table").on('keypress keyup blur change','input',function(event){
-		setStorage(); /* sets storage on keyup */
+		var tableId = $(event.target).closest('table').attr('id');
+		setStorage(tableId); /* sets storage on keyup */
 	});
 });
-function initializeFields(){
+function initializeTotal(){
 	$("#tabb4"+ " span").text('$ ' + getCost());
+}
+function initializeFields(){
+	var tableId = ['tabb1','tabb2','tabb3','tabb4'];
+	var length = tableId.length;
+	for(var i = 0; i < length; i++){
+		getStorage(tableId[i]);
+	}
 }
 function addNewRow(event){
 	 var group = event.target.id;
@@ -62,43 +71,18 @@ function getCost(){
 	}
 	return total;
 }
-
-function setStorage(){
+function setStorage(tableId){
+	var group = tableId;
 	$('input').each(function(){
 		$(this).keyup(function(){
 			$(this).attr('value', $(this).val());
 		});
 	});
-
-	var html = $('#tabb1 tbody').html();
-	localStorage.setItem('tabb1', html);
-	
-	html = $('#tabb2 tbody').html();
-	localStorage.setItem('tabb2', html);
-	
-	html = $('#tabb3 tbody').html();
-	localStorage.setItem('tabb3', html);
-	
-	html = $('#tabb4 tbody').html();
-	localStorage.setItem('tabb4', html);
+	var html = $('#' + group +' tbody').html();
+	localStorage.setItem(group, html);
 }
-
-
-function getStorage(){
-	
-	$('#tabb1 tbody').empty();
-	var html = localStorage.getItem('tabb1');
-	$('#tabb1 tbody').append(html);
-
-	$('#tabb2 tbody').empty();
-	html = localStorage.getItem('tabb2');
-	$('#tabb2 tbody').append(html);
-
-	$('#tabb3 tbody').empty();
-	html = localStorage.getItem('tabb3');
-	$('#tabb3 tbody').append(html);
-
-	$('#tabb4 tbody').empty();
-	html = localStorage.getItem('tabb4');
-	$('#tabb4 tbody').append(html);
+function getStorage(tableId){
+	var html = localStorage.getItem(tableId);
+	$('#'+tableId+' tbody').empty();
+	$('#'+tableId+ ' tbody').append(html);
 }
