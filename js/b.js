7f8640cb4e99b9bba1b93f7b164f4ecd29784754
Kevin Hu <ContactKevinHu@gmail.com>
2017-11-28 16:48:47 -0500

    finished b.js (inefficient)

--- js/b.js
+++ js/b.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	getStorage();
 	initializeFields();
 	$("#tabb4 button").on('click',function(event){
 		addNewRowGroup4('#tabb4');
@@ -6,9 +7,13 @@ $(document).ready(function(){
 	$("button").not('#tabb4').on('click', function(event) {
 		console.log("adding a new row --" + " user pressed button id " + event.target.id);
 		addNewRow(event);		/* Act on the event */
+		setStorage();
 	});
 	$("#tabb4").on('keypress keyup blur change load','input',function(event){
 		$("#tabb4"+ " span").text('$ ' + getCost()); //Set total to tabletotal span tag
+	});
+	$("table").on('keypress keyup blur change','input',function(event){
+		setStorage(); /* sets storage on keyup */
 	});
 });
 function initializeFields(){
@@ -56,4 +61,44 @@ function getCost(){
 		}
 	}
 	return total;
+}
+
+function setStorage(){
+	$('input').each(function(){
+		$(this).keyup(function(){
+			$(this).attr('value', $(this).val());
+		});
+	});
+
+	var html = $('#tabb1 tbody').html();
+	localStorage.setItem('tabb1', html);
+	
+	html = $('#tabb2 tbody').html();
+	localStorage.setItem('tabb2', html);
+	
+	html = $('#tabb3 tbody').html();
+	localStorage.setItem('tabb3', html);
+	
+	html = $('#tabb4 tbody').html();
+	localStorage.setItem('tabb4', html);
+}
+
+
+function getStorage(){
+	
+	$('#tabb1 tbody').empty();
+	var html = localStorage.getItem('tabb1');
+	$('#tabb1 tbody').append(html);
+
+	$('#tabb2 tbody').empty();
+	html = localStorage.getItem('tabb2');
+	$('#tabb2 tbody').append(html);
+
+	$('#tabb3 tbody').empty();
+	html = localStorage.getItem('tabb3');
+	$('#tabb3 tbody').append(html);
+
+	$('#tabb4 tbody').empty();
+	html = localStorage.getItem('tabb4');
+	$('#tabb4 tbody').append(html);
 }
